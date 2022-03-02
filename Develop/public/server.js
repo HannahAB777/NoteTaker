62409973696ef2bb4312122b495851d1a9bd32a1
hannah AB <hannah.allisonbatt@gmail.com>
2022-03-02 09:39:06 +0800

    fixed css by moving folders around

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 const webRoutes = require('../routes/web');
-const apiRoutes = require('./api');
+const apiRoutes = require('../routes/api');
 
 const PORT = process.env.PORT||5001;
 
+app.use(express.static('../public'));
 app.use(webRoutes);
 app.use(apiRoutes);
-app.use(express.static('public'));
 
 app.listen(PORT, () =>{
     console.log(`listening on http://localhost:${PORT}`);
