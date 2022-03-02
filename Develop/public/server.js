aae4edf52accbf37c71db20419afb561e70108a3
hannah AB <hannah.allisonbatt@gmail.com>
2022-03-02 11:09:22 +0800

    incomplete post route

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -6,6 +6,8 @@ const apiRoutes = require('../routes/api');
 const PORT = process.env.PORT||5001;
 
 app.use(express.static('../public'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.use(webRoutes);
 app.use(apiRoutes);
 
--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -3,14 +3,41 @@ const { fstat } = require('fs');
 const router = express.Router();;
 const path = require('path');
 const fs = require('fs');
+const uuid = require('uuid');
+const { json } = require('express');
+//find the db.json file
+const notesdatapath = path.join(__dirname, "db/db.json"); 
+
+/**
+ * @returns {array}
+ * **/
+
+function retrieveNotes(){
+    //collect the data from that file
+    JSON.parse(fs.readFileSync(notesdatapath, "utf-8")|| "");
+}
 
 router.get('/api/notes', (req, res) => {
-    //find the db.json file
-    const notesdatapath = path.join(__dirname, "..", "db", "db.json"); 
-    //collect the data from that file
-    const notesdata = JSON.parse(fs.readFileSync(notesdatapath, "utf-8")|| "");
+    const notesdata = retrieveNotes();
     //return the the content as a JSON
     res.json(notesdata);
+});
+
+router.post('/api/notes', (req, res) => {
+  const {title, text} = req.body;
+  console.log(title + text);
+
+  const newNote = {
+    id:uuid,
+    title,
+    text,
+}
+  const currentNotes = retrieveNotes();
+  currentNotes.push(newNote);
+  fs.readFileSync(notesdatapath, JSON.stringify(currentNotes),"utf-8");
+
+  res.json = newNote;
+
 });
 
 module.exports = router;
