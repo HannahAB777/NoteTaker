227626c260e23640b34fa5d3d3455ccd8ada51e8
hannah AB <hannah.allisonbatt@gmail.com>
2022-03-07 19:40:28 +0800

    changes

--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const webRoutes = require('../routes/web');
-const apiRoutes = require('../routes/api');
+const webRoutes = require('./routes/web');
+const apiRoutes = require('./routes/api');
 
 const PORT = process.env.PORT||5001;
 
-app.use(express.static('../public'));
+app.use(express.static('./public'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(webRoutes);
